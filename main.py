7b846817206135524fa51bf374ac137ef1261788
dakixr <dakixr@gmail.com>
2024-06-20 15:08:04 +0200

    mvp

---
+++ main.py
@@ -0,0 +1,100 @@
+from docxtpl import DocxTemplate
+
+
+tpl = DocxTemplate("template.docx")
+
+context = {
+    # First Page
+    "function": "Software Engineer",
+    # Personal Details
+    "first_name": "Daniel",
+    "surname": "Rodriguez Mariblanca",
+    "date_of_birth": "08-08-1999",
+    "city": "Luxembourg",
+    "nationality": "Spanish",
+    "availability": "Full Time",
+    "drivers_license": "Yes",
+    # Profile & Ambition
+    "profile_and_ambition": "bla bla bla " * 30,
+    "highlights": [f"This is my highlight {n}" for n in range(4)],
+    # Education & Courses
+    "education": [
+        {
+            "period": "1999-2024",
+            "name_education": "Life",
+            "name_employer_client": "My parents",
+            "status": "Ongoing",
+        }
+    ],
+    "courses": [
+        {
+            "period": "2020-2021",
+            "name_education": "Covid",
+            "name_employer_client": "A virus",
+            "status": "Finished?",
+        }
+    ],
+    # Work experience
+    "work_experience": [
+        {
+            "period": "1999-2024",
+            "function_name": "Life learner",
+            "name_employer_client": "My parents",
+            "bullet_points": [f"This is my bullet_point {n}" for n in range(4)],
+        }
+    ],
+    # Expertise
+    "expertise": [
+        {
+            "category": "Programming Languages",
+            "list": [
+                {
+                    "name": "Python",
+                    "basic": "",
+                    "good": "",
+                    "excellent": "X",
+                },
+                {
+                    "name": "Eating",
+                    "basic": "",
+                    "good": "",
+                    "excellent": "X",
+                },
+                {
+                    "name": "Sleeping",
+                    "basic": "",
+                    "good": "",
+                    "excellent": "X",
+                },
+            ],
+        },
+        {
+            "category": "Languages",
+            "list": [
+                {
+                    "name": "Spanish",
+                    "basic": "",
+                    "good": "",
+                    "excellent": "X",
+                },
+                {
+                    "name": "English",
+                    "basic": "",
+                    "good": "",
+                    "excellent": "X",
+                },
+                {
+                    "name": "German",
+                    "basic": "X",
+                    "good": "",
+                    "excellent": "",
+                },
+            ],
+        },
+    ],
+}
+
+tpl.render(context)
+tpl.save("generated_cv.docx")
+
+print("Successfully generated the CV!")
