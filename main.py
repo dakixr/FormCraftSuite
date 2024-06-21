19384875eaee0a042b25c2fd7fca92a01502d4fe
dakixr <dakixr@gmail.com>
2024-06-21 10:54:51 +0200

    save data more or less

--- main.py
+++ main.py
@@ -1,103 +1,10 @@
 
-from docxtpl import DocxTemplate, InlineImage
-from docx.shared import Mm
+from docxtpl import DocxTemplate
+
+from daniel import get_context
 
 tpl = DocxTemplate("template.docx")
-
-context = {
-    # First Page
-    "function": "Software Engineer",
-    # Personal Details
-    "first_name": "Daniel",
-    "surname": "Rodriguez Mariblanca",
-    "date_of_birth": "08-08-1999",
-    "city": "Luxembourg",
-    "nationality": "Spanish",
-    "availability": "Full Time",
-    "drivers_license": "Yes",
-    "profile_pic":  InlineImage(tpl, 'pooo.png', height=Mm(40)),
-    # Profile & Ambition
-    "profile_and_ambition": "bla bla bla " * 30,
-    "highlights": [f"This is my highlight {n}" for n in range(4)],
-    # Education & Courses
-    "education": [
-        {
-            "period": "1999-2024",
-            "name_education": "Life",
-            "name_employer_client": "My parents",
-            "status": "Ongoing",
-        }
-    ],
-    "courses": [
-        {
-            "period": "2020-2021",
-            "name_education": "Covid",
-            "name_employer_client": "A virus",
-            "status": "Finished?",
-        }
-    ],
-    # Work experience
-    "work_experience": [
-        {
-            "period": "1999-2024",
-            "function_name": "Life learner",
-            "name_employer_client": "My parents",
-            "bullet_points": [f"This is my bullet_point {n}" for n in range(4)],
-        }
-    ],
-    # Expertise
-    "expertise": [
-        {
-            "category": "Programming Languages",
-            "list": [
-                {
-                    "name": "Python",
-                    "basic": "",
-                    "good": "",
-                    "excellent": "X",
-                },
-                {
-                    "name": "Eating",
-                    "basic": "",
-                    "good": "",
-                    "excellent": "X",
-                },
-                {
-                    "name": "Sleeping",
-                    "basic": "",
-                    "good": "",
-                    "excellent": "X",
-                },
-            ],
-        },
-        {
-            "category": "Languages",
-            "list": [
-                {
-                    "name": "Spanish",
-                    "basic": "",
-                    "good": "",
-                    "excellent": "X",
-                },
-                {
-                    "name": "English",
-                    "basic": "",
-                    "good": "",
-                    "excellent": "X",
-                },
-                {
-                    "name": "German",
-                    "basic": "X",
-                    "good": "",
-                    "excellent": "",
-                },
-            ],
-        },
-    ],
-}
-
-tpl.render(context)
-tpl.replace_media('dummy_profile_pic.png', 'pooo.png')
+tpl.render(get_context(tpl))
 tpl.save("generated_cv.docx")
 
 print("Successfully generated the CV!")
--- project/app.py
+++ project/app.py
@@ -1,48 +1,85 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-import os
-from werkzeug.utils import secure_filename
+from flask import Flask, render_template, redirect, url_for, flash
 from forms import ProfileForm
-import pprint
+import json
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
-app.config['WTF_CSRF_ENABLED'] = False  # Disable CSRF protection
+app.secret_key = "your_secret_key"
+app.config["WTF_CSRF_ENABLED"] = False  # Disable CSRF protection
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET"])
 def index():
+    return render_template("form.html", form=ProfileForm())
+
+
+@app.route("/submit_form", methods=["POST"])
+def submit_form():
     form = ProfileForm()
-    if form.validate_on_submit():
-        # Save form data
-        form_data = {
-            'first_name': form.first_name.data,
-            'surname': form.surname.data,
-            'date_of_birth': form.date_of_birth.data.strftime('%d-%m-%Y %Y-%m-'),
-            'city': form.city.data,
-            'nationality': form.nationality.data,
-            'availability': form.availability.data.strftime('%d-%m-%Y'),
-            'drivers_license': form.drivers_license.data,
-            'profile_and_ambition': form.profile_and_ambition.data,
-            'highlights': [highlight.highlight.data for highlight in form.highlights],
-            'education': [{'period': edu.period.data, 'name_education': edu.name_education.data, 'name_employer_client': edu.name_employer_client.data, 'status': edu.status.data} for edu in form.education],
-            'courses': [{'period': course.period.data, 'name_education': course.name_education.data, 'name_employer_client': course.name_employer_client.data, 'status': course.status.data} for course in form.courses],
-            'work_experience': [{'period': work.period.data, 'function_name': work.function_name.data, 'name_employer_client': work.name_employer_client.data, 'bullet_points': [bp.bullet_point.data for bp in work.bullet_points]} for work in form.work_experience],
-            'expertise_programming': [{'expertise': exp.expertise.data, 'level': exp.level.data} for exp in form.expertise_programming],
-            'expertise_languages': [{'expertise': exp.expertise.data, 'level': exp.level.data} for exp in form.expertise_languages]
-        }
-        # Handle file upload
-        # if form.profile_pic.data:
-        #     profile_pic = form.profile_pic.data
-        #     filename = secure_filename(profile_pic.filename)
-        #     profile_pic.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #     form_data['profile_pic'] = filename
 
-        # Simulating save operation
-        print("Form data saved successfully!")
-        pprint.pprint(form.data)
-        flash('Form submitted successfully!', 'success')
-        return redirect(url_for('index'))
-        
-    return render_template('form.html', form=form)
+    # Save form data
+    form_data = {
+        "first_name": form.first_name.data,
+        "surname": form.surname.data,
+        "date_of_birth": form.date_of_birth.data.strftime("%d-%m-%Y"),
+        "city": form.city.data,
+        "nationality": form.nationality.data,
+        "availability": form.availability.data.strftime("%d-%m-%Y"),
+        "drivers_license": form.drivers_license.data,
+        "profile_and_ambition": form.profile_and_ambition.data,
+        "highlights": [highlight.highlight.data for highlight in form.highlights],
+        "education": [
+            {
+                "period": edu.period.data,
+                "name_education": edu.name_education.data,
+                "name_employer_client": edu.name_employer_client.data,
+                "status": edu.status.data,
+            }
+            for edu in form.education
+        ],
+        "courses": [
+            {
+                "period": course.period.data,
+                "name_education": course.name_education.data,
+                "name_employer_client": course.name_employer_client.data,
+                "status": course.status.data,
+            }
+            for course in form.courses
+        ],
+        "work_experience": [
+            {
+                "period": work.period.data,
+                "function_name": work.function_name.data,
+                "name_employer_client": work.name_employer_client.data,
+                "bullet_points": [bp.bullet_point.data for bp in work.bullet_points],
+            }
+            for work in form.work_experience
+        ],
+        "expertise_programming": [
+            {"expertise": exp.expertise.data, "level": exp.level.data}
+            for exp in form.expertise_programming
+        ],
+        "expertise_languages": [
+            {"expertise": exp.expertise.data, "level": exp.level.data}
+            for exp in form.expertise_languages
+        ],
+    }
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    # Handle file upload
+    # if form.profile_pic.data:
+    #     profile_pic = form.profile_pic.data
+    #     filename = secure_filename(profile_pic.filename)
+    #     profile_pic.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #     form_data['profile_pic'] = filename
+
+    # Simulating save operation
+    with open("data.json", "+w") as f:
+        json.dump(form_data, fp=f)
+
+    flash("Form submitted successfully!", "success")
+    print("Olee")
+
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
