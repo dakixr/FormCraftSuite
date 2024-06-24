d2ceda1f740f55409d49e419ef360f15ae3f5f15
dakixr <dakixr@gmail.com>
2024-06-24 10:44:19 +0200

    fix work_experience

--- project/app.py
+++ project/app.py
@@ -16,56 +16,6 @@ def index():
 def submit_form():
     form = ProfileForm()
 
-    # Save form data
-    form_data = {
-        "first_name": form.first_name.data,
-        "surname": form.surname.data,
-        "date_of_birth": form.date_of_birth.data.strftime("%d-%m-%Y"),
-        "city": form.city.data,
-        "nationality": form.nationality.data,
-        "availability": form.availability.data.strftime("%d-%m-%Y"),
-        "drivers_license": form.drivers_license.data,
-        "profile_and_ambition": form.profile_and_ambition.data,
-        "highlights": [highlight.highlight.data for highlight in form.highlights],
-        "education": [
-            {
-                "period": edu.period.data,
-                "name_education": edu.name_education.data,
-                "name_employer_client": edu.name_employer_client.data,
-                "status": edu.status.data,
-            }
-            for edu in form.education
-        ],
-        "courses": [
-            {
-                "period": course.period.data,
-                "name_education": course.name_education.data,
-                "name_employer_client": course.name_employer_client.data,
-                "status": course.status.data,
-            }
-            for course in form.courses
-        ],
-        "work_experience": [
-            {
-                "period": work.period.data,
-                "function_name": work.function_name.data,
-                "name_employer_client": work.name_employer_client.data,
-                "bullet_points": [bp.bullet_point.data for bp in work.bullet_points],
-            }
-            for work in form.work_experience
-        ],
-        "expertise_sections": [
-            {
-                "title": section.title.data,
-                "items": [
-                    {"expertise": item.expertise.data, "level": item.level.data}
-                    for item in section.items
-                ]
-            }
-            for section in form.expertise_sections
-        ],
-    }
-
     # # Handle file upload
     # if form.profile_pic.data:
     #     filename = secure_filename(form.profile_pic.data.filename)
@@ -75,7 +25,7 @@ def submit_form():
     # Save form_data to a file or database
     # For example, saving to a JSON file:
     with open('form_data.json', 'w') as f:
-        json.dump(form_data, f, indent=2)
+        json.dump(form.data, f, indent=2, default=str)
 
     return "Form submitted successfully!"
 
